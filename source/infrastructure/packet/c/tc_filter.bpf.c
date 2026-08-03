cecf84ca5eb587699c8bb206b40d7c1a5ba2376a
avisqqq <vasylambroz@gmail.com>
2026-08-03 19:17:13 +0200

    Branch init

--- source/infrastructure/packet/c/tc_filter.bpf.c
+++ source/infrastructure/packet/c/tc_filter.bpf.c
@@ -39,76 +39,22 @@ struct event {
     __u8 tcp_flags;
     __u8 pad[1];
 };
-
-enum event_action {
-    ACT_PASS = 0,
-    ACT_DROP = 1,
-    ACT_SKIP = 2,
-    ACT_SSH_BYPASS = 3,
-    ACT_ONLY_LOCAL_DROP = 4,
-};
-
-enum stat_key {
-    STAT_PASS = 0,
-    STAT_DROP = 1,
-    STAT_SKIP = 2,
-};
-
 struct ip_key {
     __u8 version;
     __u8 addr[16];
 };
-
-struct cidr4_key{
-    __u32 prefixlen;
-    __u8 addr[4]; 
+enum event_action {
+    ACT_PASS = 0,
 };
 
-struct cidr6_key{
-    __u32 prefixlen;
-    __u8 addr[16]; 
+enum stat_key {
+    STAT_ASSUMED= 0,
+    STAT_NOT_ASSUMED = 1,
 };
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, struct ip_key);
-    __type(value, __u8);
-} onlylocal SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_LPM_TRIE);
-    __uint(max_entries, 16); // ??
-    __uint(map_flags, BPF_F_NO_PREALLOC);
-    __type(key, struct cidr4_key);
-    __type(value, __u8);
-} local_nets_v4 SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_LPM_TRIE);
-    __uint(max_entries, 16); // ??
-    __uint(map_flags, BPF_F_NO_PREALLOC);
-    __type(key, struct cidr6_key);
-    __type(value, __u8);
-} local_nets_v6 SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, struct ip_key);
-    __type(value, __u8);
-} blacklist SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, struct ip_key);
-    __type(value, __u8);
-} whitelist SEC(".maps");
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 3);
+    __uint(max_entries, 2);
     __type(key, __u32);
     __type(value, __u64);
 } stats SEC(".maps");
@@ -153,24 +99,6 @@ static __always_inline void fill_ipv6_key(struct ip_key *key, const struct in6_a
     __builtin_memset(key, 0, sizeof(*key));
     key->version = 6;
     __builtin_memcpy(key->addr, addr, 16);
-}
-
-static __always_inline int dst_is_in_local_v6(const struct in6_addr *dst){
-    struct cidr6_key key = {};
-
-    key.prefixlen = 128;
-    __builtin_memcpy(key.addr, dst, 16);
-
-    return bpf_map_lookup_elem(&local_nets_v6, &key) != 0;
-}
-
-static __always_inline int dst_is_in_local_v4(__u32 dst){
-    struct cidr4_key key = {};
-
-    key.prefixlen = 32;
-    __builtin_memcpy(key.addr, &dst, 4);
-
-    return bpf_map_lookup_elem(&local_nets_v4, &key) != 0;
 }
 
 static __always_inline void emit_event(
@@ -220,13 +148,22 @@ static __always_inline int handle_ipv4(
         __u8 direction) {
     struct iphdr *ip = (void *)(eth + 1);
     if ((void *)(ip + 1) > data_end)
+    {
+        inc_stat(STAT_NOT_ASSUMED);
         return TC_ACT_OK;
+    }
 
     __u32 ip_hdr_len = ip->ihl * 4;
     if (ip_hdr_len < sizeof(*ip))
+    {
+        inc_stat(STAT_NOT_ASSUMED);
         return TC_ACT_OK;
+    }
     if ((void *)ip + ip_hdr_len > data_end)
+    {
+        inc_stat(STAT_NOT_ASSUMED);
         return TC_ACT_OK;
+    }
 
     __u32 src = ip->saddr;
     __u32 dst = ip->daddr;
@@ -237,57 +174,28 @@ static __always_inline int handle_ipv4(
 
     if (ip->protocol == IPPROTO_TCP) {
         struct tcphdr *tcp = (void *)ip + ip_hdr_len;
-        if ((void *)(tcp + 1) > data_end)
-            return TC_ACT_OK;
+        if ((void *)(tcp + 1) > data_end){
+        inc_stat(STAT_NOT_ASSUMED);
+        return TC_ACT_OK;
+    }
         src_port  = bpf_ntohs(tcp->source);
         dst_port  = bpf_ntohs(tcp->dest);
         tcp_flags = ((__u8 *)tcp)[13]; // flags byte: CWR|ECE|URG|ACK|PSH|RST|SYN|FIN
     } else if (ip->protocol == IPPROTO_UDP) {
         struct udphdr *udp = (void *)ip + ip_hdr_len;
-        if ((void *)(udp + 1) > data_end)
-            return TC_ACT_OK;
+        if ((void *)(udp + 1) > data_end) {
+        inc_stat(STAT_NOT_ASSUMED);
+        return TC_ACT_OK;}
         src_port = bpf_ntohs(udp->source);
         dst_port = bpf_ntohs(udp->dest);
-    }
+}
 // for lookup
     struct ip_key src_key, dst_key;
     fill_ipv4_key(&src_key, src);
     fill_ipv4_key(&dst_key, dst);
 
-// Check for local net
-    if (bpf_map_lookup_elem(&onlylocal, &src_key)){
-        if(!dst_is_in_local_v4(dst)){
-        inc_stat(STAT_DROP);
-        emit_event((const __u8 *)&src, (const __u8 *)&dst, 4, ip->protocol,
-                   ACT_ONLY_LOCAL_DROP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_SHOT;
-        }
-    }
 
-    if (ip->protocol == IPPROTO_TCP && (src_port == 22 || dst_port == 22)) {
-        emit_event((const __u8 *)&src, (const __u8 *)&dst, 4, ip->protocol,
-                   ACT_SSH_BYPASS, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_OK;
-    }
-
-
-    if (bpf_map_lookup_elem(&blacklist, &src_key) ||
-        bpf_map_lookup_elem(&blacklist, &dst_key)) {
-        inc_stat(STAT_DROP);
-        emit_event((const __u8 *)&src, (const __u8 *)&dst, 4, ip->protocol,
-                   ACT_DROP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_SHOT;
-    }
-
-    if (bpf_map_lookup_elem(&whitelist, &src_key) ||
-        bpf_map_lookup_elem(&whitelist, &dst_key)) {
-        inc_stat(STAT_SKIP);
-        emit_event((const __u8 *)&src, (const __u8 *)&dst, 4, ip->protocol,
-                   ACT_SKIP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_OK;
-    }
-
-    inc_stat(STAT_PASS);
+    inc_stat(STAT_ASSUMED);
     emit_event((const __u8 *)&src, (const __u8 *)&dst, 4, ip->protocol,
                ACT_PASS, direction, src_port, dst_port, pkt_size, tcp_flags);
     return TC_ACT_OK;
@@ -299,7 +207,10 @@ static __always_inline int handle_ipv6(
         __u8 direction) {
     struct ipv6hdr *ipv6 = (void *)(eth + 1);
     if ((void *)(ipv6 + 1) > data_end)
+{
+    inc_stat(STAT_NOT_ASSUMED);
         return TC_ACT_OK;
+}
 
     __u16 pkt_size  = bpf_ntohs(ipv6->payload_len) + sizeof(*ipv6);
     __u16 src_port  = 0, dst_port = 0;
@@ -308,14 +219,20 @@ static __always_inline int handle_ipv6(
     if (ipv6->nexthdr == IPPROTO_TCP) {
         struct tcphdr *tcp = (void *)(ipv6 + 1);
         if ((void *)(tcp + 1) > data_end)
-            return TC_ACT_OK;
+{
+    inc_stat(STAT_NOT_ASSUMED);
+        return TC_ACT_OK;
+}
         src_port  = bpf_ntohs(tcp->source);
         dst_port  = bpf_ntohs(tcp->dest);
         tcp_flags = ((__u8 *)tcp)[13];
     } else if (ipv6->nexthdr == IPPROTO_UDP) {
         struct udphdr *udp = (void *)(ipv6 + 1);
-        if ((void *)(udp + 1) > data_end)
+        if ((void *)(udp + 1) > data_end) {
+            inc_stat(STAT_NOT_ASSUMED);
             return TC_ACT_OK;
+        }
+
         src_port = bpf_ntohs(udp->source);
         dst_port = bpf_ntohs(udp->dest);
     }
@@ -324,39 +241,8 @@ static __always_inline int handle_ipv6(
     fill_ipv6_key(&src_key, &ipv6->saddr);
     fill_ipv6_key(&dst_key, &ipv6->daddr);
 
-//check local
-if (bpf_map_lookup_elem(&onlylocal, &src_key)){
-        if(!dst_is_in_local_v6(&ipv6->daddr)){
-        inc_stat(STAT_DROP);
-        emit_event((const __u8 *)&ipv6->saddr, (const __u8 *)&ipv6->daddr, 6, ipv6->nexthdr,
-                   ACT_ONLY_LOCAL_DROP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_SHOT;
-        }
-    }
 
-    if (ipv6->nexthdr == IPPROTO_TCP && (src_port == 22 || dst_port == 22)) {
-        emit_event((const __u8 *)&ipv6->saddr, (const __u8 *)&ipv6->daddr, 6, ipv6->nexthdr,
-                   ACT_SSH_BYPASS, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_OK;
-    }
-
-    if (bpf_map_lookup_elem(&blacklist, &src_key) ||
-        bpf_map_lookup_elem(&blacklist, &dst_key)) {
-        inc_stat(STAT_DROP);
-        emit_event((const __u8 *)&ipv6->saddr, (const __u8 *)&ipv6->daddr, 6, ipv6->nexthdr,
-                   ACT_DROP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_SHOT;
-    }
-
-    if (bpf_map_lookup_elem(&whitelist, &src_key) ||
-        bpf_map_lookup_elem(&whitelist, &dst_key)) {
-        inc_stat(STAT_SKIP);
-        emit_event((const __u8 *)&ipv6->saddr, (const __u8 *)&ipv6->daddr, 6, ipv6->nexthdr,
-                   ACT_SKIP, direction, src_port, dst_port, pkt_size, tcp_flags);
-        return TC_ACT_OK;
-    }
-
-    inc_stat(STAT_PASS);
+    inc_stat(STAT_ASSUMED);
     emit_event((const __u8 *)&ipv6->saddr, (const __u8 *)&ipv6->daddr, 6, ipv6->nexthdr,
                ACT_PASS, direction, src_port, dst_port, pkt_size, tcp_flags);
     return TC_ACT_OK;
